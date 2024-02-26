eeea0694cea896286aa8d6ef8940cb9b1aa9718a
Dominik Labudzinski <dominik@labudzinski.com>
2024-02-26 11:54:05 +0100

    getSmallPreviewConfig

--- src/Service/ThumbnailService/Document.php
+++ src/Service/ThumbnailService/Document.php
@@ -129,7 +129,7 @@ class Document implements ServiceInterface
         }
 
         if ($request->get('treepreview')) {
-            $thumbnail = Asset\Image\Thumbnail\Config::getPreviewConfig();
+            $thumbnail = Asset\Image\Thumbnail\Config::getSmallPreviewConfig();
         }
 
         $page = 1;
