d3c85734097b9f2efc2c40bcbf731a1f6c333380
Dominik Labudzinski <dominik@labudzinski.com>
2024-03-12 11:26:35 +0100

    thumbnails

---
+++ src/Service/AssetGridService.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace Pimcore\Bundle\AdminBundle\Service;
+
+use League\Flysystem\FilesystemException;
+use Pimcore\Loader\ImplementationLoader\Exception\UnsupportedException;
+use Pimcore\Logger;
+use Pimcore\Model\Asset;
+use Pimcore\Model\Asset\MetaData\ClassDefinition\Data\Data;
+use Pimcore\Model\Asset\Service;
+use Pimcore\Config;
+use Pimcore\Event\AssetEvents;
+use Pimcore\Event\Model\AssetEvent;
+use Pimcore\Model;
+use Pimcore\Model\Asset\Image\Thumbnail\Config as ThumbnailConfig;
+use Pimcore\Model\Asset\Image\ThumbnailInterface;
+use Pimcore\Model\Element;
+use Pimcore\Model\Element\ElementInterface;
+use Pimcore\Model\Tool\TmpStore;
+use Pimcore\Tool\Storage;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+use Symfony\Component\HttpKernel\EventListener\AbstractSessionListener;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+class AssetGridService
+{
+    /**
+     * @throws FilesystemException
+     */
+    public static function gridAssetData(Asset $asset, array $fields = null, string $requestedLanguage = null, array $params = []): array
+    {
+        $data = Element\Service::gridElementData($asset);
+        $loader = null;
+
+        if ($asset instanceof Asset && !empty($fields)) {
+            $data = [
+                'id' => $asset->getId(),
+                'id~system' => $asset->getId(),
+                'type~system' => $asset->getType(),
+                'fullpath~system' => $asset->getRealFullPath(),
+                'filename~system' => $asset->getKey(),
+                'creationDate~system' => $asset->getCreationDate(),
+                'modificationDate~system' => $asset->getModificationDate(),
+                'idPath~system' => Element\Service::getIdPath($asset),
+            ];
+
+            $requestedLanguage = str_replace('default', '', $requestedLanguage);
+
+            foreach ($fields as $field) {
+                $fieldDef = explode('~', $field);
+                if (isset($fieldDef[1]) && $fieldDef[1] === 'system') {
+                    if ($fieldDef[0] === 'preview') {
+                        switch ($asset) {
+                            case $asset instanceof Asset\Image:
+                                $thumbnailUrl = ThumbnailLinkService::getImage($asset->getId());
+                                if($thumbnailUrl === null) {
+                                    $thumbnailUrl = Service::getPreviewThumbnail($asset, ['treepreview' => true, 'width' => 108, 'height' => 70, 'frame' => true]);
+                                }
+
+                                break;
+                            case $asset instanceof Asset\Folder:
+                                $thumbnailUrl = ThumbnailLinkService::getFolder($asset->getId());
+                                if($thumbnailUrl === null) {
+                                    $thumbnailUrl = Service::getPreviewThumbnail($asset, ['treepreview' => true, 'width' => 108, 'height' => 70, 'frame' => true]);
+                                }
+
+                                break;
+                            case $asset instanceof Asset\Video && \Pimcore\Video::isAvailable():
+                                $thumbnailUrl = ThumbnailLinkService::getVideo($asset->getId());
+                                if($thumbnailUrl === null) {
+                                    $thumbnailUrl = Service::getPreviewThumbnail($asset, ['treepreview' => true, 'width' => 108, 'height' => 70, 'frame' => true]);
+                                }
+
+                                break;
+                            case $asset instanceof Asset\Document && \Pimcore\Document::isAvailable() && $asset->getPageCount():
+                                $thumbnailUrl = ThumbnailLinkService::getDocument($asset->getId());
+                                if($thumbnailUrl === null) {
+                                    $thumbnailUrl = Service::getPreviewThumbnail($asset, ['treepreview' => true, 'width' => 108, 'height' => 70, 'frame' => true]);
+                                }
+
+                                break;
+                            case $asset instanceof Asset\Audio:
+                                $thumbnailUrl = '/bundles/pimcoreadmin/img/flat-color-icons/speaker.svg';
+
+                                break;
+                            default:
+                                $thumbnailUrl = '/bundles/pimcoreadmin/img/filetype-not-supported.svg';
+                        }
+                        $data[$field] = $thumbnailUrl;
+                    } elseif ($fieldDef[0] === 'size') {
+                        $size = $asset->getFileSize();
+                        $data[$field] = formatBytes($size);
+                    }
+                } else {
+                    if (isset($fieldDef[1])) {
+                        $language = ($fieldDef[1] === 'none' ? '' : $fieldDef[1]);
+                        $rawMetaData = $asset->getMetadata($fieldDef[0], $language, true, true);
+                    } else {
+                        $rawMetaData = $asset->getMetadata($field, $requestedLanguage, true, true);
+                    }
+
+                    $metaData = $rawMetaData['data'] ?? null;
+
+                    if ($rawMetaData) {
+                        $type = $rawMetaData['type'];
+                        if (!$loader) {
+                            $loader = \Pimcore::getContainer()->get('pimcore.implementation_loader.asset.metadata.data');
+                        }
+
+                        $metaData = $rawMetaData['data'] ?? null;
+
+                        try {
+                            /** @var Data $instance */
+                            $instance = $loader->build($type);
+                            $metaData = $instance->getDataForListfolderGrid($rawMetaData['data'] ?? null, $rawMetaData);
+                        } catch (UnsupportedException $e) {
+                        }
+                    }
+
+                    $data[$field] = $metaData;
+                }
+            }
+        }
+
+        return $data;
+    }
+}
